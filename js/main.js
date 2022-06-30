fac38ddf697944fe34f3ba928cd7b29d66d86b32
Marcello Fitton <marcellofitton@gmail.com>
2022-06-29 23:02:17 -0700

    First Commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const choices = ['rock', 'paper', 'scissors']
+
+
+function game() {
+  //play the game
+  //play five rounds
+  //console based
+}
+
+function playRound(playerSelection, computerSelection) {
+    const playerSelection = playerChoice()
+}
+
+function playerChoice() {
+    //receive input from player
+}
+
+function computerChoice() {
+//random input from computer
+return choices[Math.floor(Math.random()*3)]
+
+}
